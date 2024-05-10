ffa2508d6bd0b10d6598c27093fb70e8c18cafe8
An Phan Hoài <phoaianvlogs@gmail.com>
2024-05-10 20:38:58 +0700

    Fix: CORS

--- gateway/src/main/java/vn/edu/iuh/GatewayApplication.java
+++ gateway/src/main/java/vn/edu/iuh/GatewayApplication.java
@@ -2,33 +2,11 @@ package vn.edu.iuh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
-	}
-
-	@Bean
-	public CorsFilter corsFilter() {
-		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		final CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true); // Allows credentials, cookies, or authentication data to be included in requests
-		config.addAllowedOriginPattern("*"); // Allows requests from any origin
-		config.addAllowedHeader("*"); // Allows all headers
-		config.addAllowedMethod("OPTIONS");
-		config.addAllowedMethod("HEAD");
-		config.addAllowedMethod("GET");
-		config.addAllowedMethod("PUT");
-		config.addAllowedMethod("POST");
-		config.addAllowedMethod("DELETE");
-		config.addAllowedMethod("PATCH");
-		source.registerCorsConfiguration("/**", config);
-		return new CorsFilter(source);
 	}
 }
