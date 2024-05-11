f95db19baf8c4041139b8898033a60ec921f1def
Tuan2110 <104837107+Tuan2110@users.noreply.github.com>
2024-05-11 12:33:04 +0700

    fix : cors

--- gateway/src/main/java/vn/edu/iuh/GatewayApplication.java
+++ gateway/src/main/java/vn/edu/iuh/GatewayApplication.java
@@ -2,6 +2,8 @@ package vn.edu.iuh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class GatewayApplication {
